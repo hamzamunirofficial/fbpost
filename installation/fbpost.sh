a90d65aa8e183df0b0238c2687557985ce69c6f9
BCoder <77330522+bcoder909@users.noreply.github.com>
2021-01-14 22:13:12 +0500

    Update fbpost.sh

--- installation/fbpost.sh
+++ installation/fbpost.sh
@@ -1,21 +1,27 @@
 #!/bin/bash
-$(clear)
+clear
 path=$(pwd)
 FILE='/data/data/com.termux/files/usr/etc/bash.bashrc'
 PKG='/data/data/com.termux/files/usr/bin/getconf'
 if [ ! -f "$PKG" ]; then
-    $(pkg install getconf)
+        pkg install getconf
+        clear
 fi
 bit=$(getconf LONG_BIT)
-$(cd /data/data/com.termux/files/usr/lib)
-$(wget https://raw.githubusercontent.com/bcoder909/fbpost/main/binaries/for-termux/${bit}bit/fbpost.so)
-$(chmod 777 /data/data/com.termux/files/usr/lib/fbpost.so)
+cd /data/data/com.termux/files/usr/lib
+wget https://raw.githubusercontent.com/bcoder909/fbpost/main/binaries/for-termux/${bit}bit/fbpost.so
+clear
+chmod 777 /data/data/com.termux/files/usr/lib/fbpost.so
 if [ -f "$FILE" ]; then
-    $(echo "alias fbpost='python3 -m /data/data/com.termux/files/usr/lib/fbpost.so'" >> ${FILE})
+        echo "alias fbpost='python3 -m /data/data/com.termux/files/usr/lib/fbpost.so'" >> ${FILE}
+        clear
+else
+        touch ${FILE} && echo "alias fbpost='python3 -m /data/data/com.termux/files/usr/lib/fbpost.so'" >> ${FILE}
+        clear
 fi
-$(cd ${path})
-$(rm -rf fbpost.so fbpost.sh)
-$(source ${FILE})
+cd ${path}
+rm -rf fbpost.so fbpost.sh
+source ${FILE}
 echo ''
 echo ''
 echo ''
