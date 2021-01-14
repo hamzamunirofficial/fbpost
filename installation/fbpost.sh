91cd7e5b404502e548c588140fb83b9c4024de84
BCoder <77330522+bcoder909@users.noreply.github.com>
2021-01-14 21:59:06 +0500

    Add files via upload

--- installation/fbpost.sh
+++ installation/fbpost.sh
@@ -1,1 +1,25 @@
-
+#!/bin/bash
+$(clear)
+path=$(pwd)
+FILE='/data/data/com.termux/files/usr/etc/bash.bashrc'
+PKG='/data/data/com.termux/files/usr/bin/getconf'
+if [ ! -f "$PKG" ]; then
+    $(pkg install getconf)
+fi
+bit=$(getconf LONG_BIT)
+$(cd /data/data/com.termux/files/usr/lib)
+$(wget https://raw.githubusercontent.com/bcoder909/fbpost/main/binaries/for-termux/${bit}bit/fbpost.so)
+$(chmod 777 /data/data/com.termux/files/usr/lib/fbpost.so)
+if [ -f "$FILE" ]; then
+    $(echo "alias fbpost='python3 -m /data/data/com.termux/files/usr/lib/fbpost.so'" >> ${FILE})
+fi
+$(cd ${path})
+$(rm -rf fbpost.so fbpost.sh)
+$(source ${FILE})
+echo ''
+echo ''
+echo ''
+echo ' fbpost tool has been installed'
+echo ' To run tool just put command fbpost'
+echo ' Thanks for installing'
+echo ''
